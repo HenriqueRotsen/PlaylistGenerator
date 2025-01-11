e77c0efa8d214a9cd7e3db31b93d678c85d6b4da
HenriqueRotsen <henriquerotsensf@gmail.com>
2025-01-11 09:01:56 -0300

    Adaptando o código e o program.yaml para conectar com o ArgoCD

--- program/rules_generator.py
+++ program/rules_generator.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import pickle
 import ssl
+import os
 from fpgrowth_py import fpgrowth
+
 
 class PlaylistRulesGenerator:
     def __init__(self, min_sup_ratio=0.4, min_conf=0.6):
@@ -13,40 +15,47 @@ class PlaylistRulesGenerator:
         self.min_sup_ratio = min_sup_ratio
         self.min_conf = min_conf
         self.rules = []
-    
+
     def generate_rules(self, transactions):
         """
         Gera regras de associação a partir de transações.
         :param transactions: Lista de listas, onde cada sublista representa uma cesta de itens.
         """
-        freq_item_set, self.rules = fpgrowth(transactions, self.min_sup_ratio, self.min_conf)
+        freq_item_set, self.rules = fpgrowth(
+            transactions, self.min_sup_ratio, self.min_conf
+        )
         print(self.rules)
         return freq_item_set, self.rules
-    
+
     def save_rules(self, filepath):
         """
         Salva as regras geradas em um arquivo utilizando pickle.
         :param filepath: Caminho do arquivo onde as regras serão armazenadas.
         """
-        with open(filepath, 'wb') as file:
+        with open(filepath, "wb") as file:
             pickle.dump(self.rules, file)
         print(f"Regras salvas em: {filepath}")
-    
+
 
 # Exemplo de uso
 if __name__ == "__main__":
+    # Substitui o contexto HTTPS padrão por um contexto não verificado, permitindo conexões HTTPS sem verificar certificados SSL.
     ssl._create_default_https_context = ssl._create_unverified_context
 
-    data = pd.read_csv("https://homepages.dcc.ufmg.br/~cunha/hosted/cloudcomp-2023s2-datasets/2023_spotify_ds1.csv", usecols=[6, 7])
+    # Obtém o valor da variável de ambiente chamada 'DATASET' e o armazena na variável DATA.
+    DATA = os.environ.get("DATASET")
 
-    # Exemplo de transações
-    transactions1 = data.groupby('pid')['track_name'].apply(list).tolist()
+    # Lê um arquivo CSV especificado no caminho armazenado em DATA.
+    data = pd.read_csv(DATA, usecols=[6, 7])
+
+    # Agrupa os dados no DataFrame pelo valor único da coluna 'pid'.Para cada grupo, agrega os valores da coluna 'track_name' em uma lista.
+    transactions = data.groupby("pid")["track_name"].apply(list).tolist()
 
     # Inicializando o gerador de regras
     generator = PlaylistRulesGenerator(min_sup_ratio=0.05, min_conf=0.15)
-    
+
     # Gerando regras
-    frequent_itemsets, rules = generator.generate_rules(transactions1)
+    frequent_itemsets, rules = generator.generate_rules(transactions)
 
     # Salvando regras
     generator.save_rules("./program/rules.pkl")
